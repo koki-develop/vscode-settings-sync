9ca5a8eb5aec6645743c8e3c1426be39fbbe8609
koki-develop <kou.pg.0131@gmail.com>
2024-11-16 21:03:31 +0900

    Add progress logging

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -1,6 +1,6 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import type * as vscode from "vscode";
+import * as vscode from "vscode";
 import { sh } from "./util";
 import {
   ask,
@@ -16,76 +16,112 @@ import {
 } from "./vscode";
 
 export const downloadSettings = async (context: vscode.ExtensionContext) => {
-  const sourceRepository = await _loadSourceRepository();
-  const sourceRepositoryPath = getSourceRepositoryPath(context);
-  const githubToken = await _loadGitHubToken(context);
+  await vscode.window
+    .withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: "Downloading Settings",
+      },
+      async (progress) => {
+        progress.report({ message: "Preparing..." });
 
-  await _prepareSourceRepository({
-    path: sourceRepositoryPath,
-    githubToken,
-    repository: sourceRepository,
-  });
+        const sourceRepository = await _loadSourceRepository();
+        const sourceRepositoryPath = getSourceRepositoryPath(context);
+        const githubToken = await _loadGitHubToken(context);
 
-  const settingsJson = fs.readFileSync(
-    path.join(sourceRepositoryPath, "settings.json"),
-    "utf8",
-  );
-  await writeSettingsJson(context, settingsJson);
+        await _prepareSourceRepository({
+          path: sourceRepositoryPath,
+          githubToken,
+          repository: sourceRepository,
+        });
 
-  const extensionsJson = fs.readFileSync(
-    path.join(sourceRepositoryPath, "extensions.json"),
-    "utf8",
-  );
-  const installedExtensionIds = listExtensions().map(
-    (extension) => extension.id,
-  );
-  const extensionIdsToInstall = JSON.parse(extensionsJson);
+        progress.report({ message: "Loading settings.json..." });
 
-  // install extensions
-  for (const extensionId of extensionIdsToInstall) {
-    if (installedExtensionIds.includes(extensionId)) {
-      continue;
-    }
-    await installExtension(extensionId);
-  }
+        const settingsJson = fs.readFileSync(
+          path.join(sourceRepositoryPath, "settings.json"),
+          "utf8",
+        );
+        await writeSettingsJson(context, settingsJson);
 
-  // uninstall extensions
-  for (const extensionId of installedExtensionIds) {
-    if (extensionIdsToInstall.includes(extensionId)) {
-      continue;
-    }
-    await uninstallExtension(extensionId);
-  }
+        progress.report({ message: "Loading extensions.json..." });
+
+        const extensionsJson = fs.readFileSync(
+          path.join(sourceRepositoryPath, "extensions.json"),
+          "utf8",
+        );
+        const extensionIdsToInstall = JSON.parse(extensionsJson);
+        const installedExtensionIds = listExtensions().map(
+          (extension) => extension.id,
+        );
+
+        // install extensions
+        for (const extensionId of extensionIdsToInstall) {
+          if (installedExtensionIds.includes(extensionId)) {
+            continue;
+          }
+          progress.report({ message: `Installing ${extensionId}...` });
+          await installExtension(extensionId);
+        }
+
+        // uninstall extensions
+        for (const extensionId of installedExtensionIds) {
+          if (extensionIdsToInstall.includes(extensionId)) {
+            continue;
+          }
+          progress.report({ message: `Uninstalling ${extensionId}...` });
+          await uninstallExtension(extensionId);
+        }
+      },
+    )
+    .then(() => {
+      vscode.window.showInformationMessage("Settings downloaded successfully!");
+    });
 };
 
 export const uploadSettings = async (context: vscode.ExtensionContext) => {
-  const sourceRepository = await _loadSourceRepository();
-  const sourceRepositoryPath = getSourceRepositoryPath(context);
-  const githubToken = await _loadGitHubToken(context);
+  await vscode.window
+    .withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: "Uploading Settings",
+      },
+      async (progress) => {
+        progress.report({ message: "Preparing..." });
 
-  await _prepareSourceRepository({
-    path: sourceRepositoryPath,
-    githubToken,
-    repository: sourceRepository,
-  });
+        const sourceRepository = await _loadSourceRepository();
+        const sourceRepositoryPath = getSourceRepositoryPath(context);
+        const githubToken = await _loadGitHubToken(context);
 
-  const settingsJson = await readSettingsJson(context);
-  fs.writeFileSync(
-    path.join(sourceRepositoryPath, "settings.json"),
-    settingsJson,
-  );
-  sh("git add settings.json", { cwd: sourceRepositoryPath });
+        await _prepareSourceRepository({
+          path: sourceRepositoryPath,
+          githubToken,
+          repository: sourceRepository,
+        });
 
-  const extensionIds = listExtensions().map((extension) => extension.id);
-  const extensionsJson = JSON.stringify(extensionIds, null, 2);
-  fs.writeFileSync(
-    path.join(sourceRepositoryPath, "extensions.json"),
-    extensionsJson,
-  );
-  sh("git add extensions.json", { cwd: sourceRepositoryPath });
+        const settingsJson = await readSettingsJson(context);
+        fs.writeFileSync(
+          path.join(sourceRepositoryPath, "settings.json"),
+          settingsJson,
+        );
+        sh("git add settings.json", { cwd: sourceRepositoryPath });
 
-  sh(`git commit -m "Save Settings"`, { cwd: sourceRepositoryPath });
-  sh("git push origin main --force", { cwd: sourceRepositoryPath });
+        const extensionIds = listExtensions().map((extension) => extension.id);
+        const extensionsJson = JSON.stringify(extensionIds, null, 2);
+        fs.writeFileSync(
+          path.join(sourceRepositoryPath, "extensions.json"),
+          extensionsJson,
+        );
+        sh("git add extensions.json", { cwd: sourceRepositoryPath });
+
+        progress.report({ message: "Uploading to GitHub..." });
+
+        sh(`git commit -m "Save Settings"`, { cwd: sourceRepositoryPath });
+        sh("git push origin main --force", { cwd: sourceRepositoryPath });
+      },
+    )
+    .then(() => {
+      vscode.window.showInformationMessage("Settings uploaded successfully!");
+    });
 };
 
 const _loadSourceRepository = async () => {
