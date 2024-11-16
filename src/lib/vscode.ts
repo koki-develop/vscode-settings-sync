79bacfef9c3618a08cf5b1c41227ffbbb96cb916
koki-develop <kou.pg.0131@gmail.com>
2024-11-16 15:48:07 +0900

    Format

--- src/lib/vscode.ts
+++ src/lib/vscode.ts
@@ -1,6 +1,6 @@
-import * as vscode from "vscode";
 import * as fs from "node:fs";
 import * as path from "node:path";
+import * as vscode from "vscode";
 
 export const registerCommand = (
   context: vscode.ExtensionContext,
